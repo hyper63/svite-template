8cacb6f0cf467cda357eb25ef1c499dfa0c16f55
Tom Wilson <twilson63@gmail.com>
2021-01-13 12:33:28 -0500

    added welcome page

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,24 @@
-<h1>Hello World</h1>
+<main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+  <div class="text-center">
+    <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+      <span class="block xl:inline">Hello</span>
+      <span class="block text-yellow-300 xl:inline">World</span>
+    </h1>
+    <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+      Congrats! You have just setup a SVITE + SVELTE + TAILWIND Template, now time to get busy coding!
+    </p>
+    <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+      <div class="rounded-md shadow">
+        <a href="https://svelte.dev" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-300 hover:bg-yellow-700 md:py-4 md:text-lg md:px-10">
+          Learn Svelte 
+        </a>
+      </div>
+      <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+        <a href="https://tailwindcss.com" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-yellow-500 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10">
+          Learn Tailwindcss 
+        </a>
+      </div>
+    </div>
+  </div>
+</main>
+
